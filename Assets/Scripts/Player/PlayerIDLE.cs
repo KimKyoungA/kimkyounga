5834b243fd3698018389be7341d660016d0d6cdf
KimKyoungA <44721585+KimKyoungA@users.noreply.github.com>
2018-11-03 17:03:31 +0900

    Add files via upload
    day1

---
+++ Assets/Scripts/Player/PlayerIDLE.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIDLE : PlayerFSMState
+{
+
+	// Use this for initialization(초기화)
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame(프레임마다 반복)
+	void Update () {
+        Debug.Log("IDLE");
+	}
+}
